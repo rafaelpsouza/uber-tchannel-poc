82d8552adfa4ba1c1c54e4c19f3f5b41d951ae1b
Rafael Souza <rafaelpsouza.eng@gmail.com>
2016-07-13 10:37:13 -0300

    uber tchannel poc

---
+++ src/main/java/br/eng/rafaelpsouza/Client.java
@@ -0,0 +1,41 @@
+package br.eng.rafaelpsouza;
+
+import com.uber.tchannel.api.TChannel;
+import com.uber.tchannel.api.TFuture;
+import com.uber.tchannel.messages.RawRequest;
+import com.uber.tchannel.messages.RawResponse;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.concurrent.ExecutionException;
+
+/**
+ *
+ * @author Rafael Souza
+ */
+public class Client {
+    
+    public static void main(String[] args) throws UnknownHostException, InterruptedException, ExecutionException {
+        // create another TChannel for client.
+        TChannel client = new TChannel.Builder("ping-client").build();
+        RawRequest request = new RawRequest.Builder("ping-server", "ping-handler")
+                .setHeader("Marco")
+                .setBody("Ping!")
+                .build();
+
+        // make an asynchronous request
+        TFuture<RawResponse> responseFuture = client
+                .makeSubChannel("ping-server").send(
+                request,
+                InetAddress.getByName("127.0.0.1"),
+                8080
+        );
+
+        // block and wait for the response
+        try (RawResponse response = responseFuture.get()) {
+            System.out.println(response);
+        }
+         client.shutdown();
+        
+    }
+    
+}
